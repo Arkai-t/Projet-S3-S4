374f84e152d2720e1fab3608920bafc6ec4601b4
Arkait53 <57949645+Arkait53@users.noreply.github.com>
2020-01-28 21:49:08 +0100

    Ajout de la classe gérant PSR

---
+++ InterfaceEleve/ClassLancementPSR.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 28 18:51:51 2020
+
+@author: Louison VINCENT
+"""
+
+import time
+import subprocess
+import zipfile
+import os
+
+class LancementPSR:
+    """
+    """
+
+    def __init__(self, cheminEnregistrement, nomFichier):
+        """
+        Définit le chemin où les fichiers produits vont être enregistrées et le nom du fichier enregistré
+        """
+        self.cheminEnregistrement = cheminEnregistrement
+        self.nomFichier = nomFichier
+
+    @property
+    def sortieFichier(self):
+        return r'{0}\{1}.zip'.format(self.cheminEnregistrement, self.nomFichier)
+
+    def lancement(self):
+        """
+        Lance le logiciel PSR avec comme paramètres:
+            /start : lancer PSR
+            /arcxml 1 : produire un fichier de traces XML
+            /sc 0 : ne pas faire de capture d'écran
+            /gui 0 : cacher l'interface de PSR
+            /output dossier de sortie des fichiers
+        """
+
+        if self.nomFichier.length > 5:
+            print("Le nom du fichier est trop long !")
+
+        #Formatage de la sortie comme une expression brute
+
+
+        subprocess.Popen(["psr" , "/start" , "/arcxml", "1" , "/sc", "0", "/gui", "0" , "/output" , self.sortieFichier], shell=True)
+
+    def arret(self):
+        """
+        Arrête PSR, et prépare les fichiers produits pour la fusion avec BKL
+        """
+        subprocess.Popen(["psr" , "/stop"],  shell=True)
+
+        #Attendre que le fichier se créer avant de le déziper
+        time.sleep(3)
+
+        #Dézipage
+        with zipfile.ZipFile(self.sortieFichier, 'r') as zip_ref:
+            zip_ref.extractall(r"{0}\unzip".format(self.cheminEnregistrement)) #CHOISIR UN NOM DE DOSSIER
+
+        #Supprimer .HTM
+        dossier = '{0}\\unzip'.format(self.cheminEnregistrement)
+        listeDocs = os.listdir(dossier)
+
+        #Parcours des fichiers pour supprimer tous les .MHT
+        for fichier in listeDocs:
+            if fichier.endswith(".mht"):
+                os.remove(os.path.join(dossier, fichier))
+                print(fichier," supprimé avec succès")
