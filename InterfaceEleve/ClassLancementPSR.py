1959528e6dc67b00b35c45de5c985eb8b8b8b406
Arkait53 <57949645+Arkait53@users.noreply.github.com>
2020-02-01 10:04:22 +0100

    Update ClassLancementPSR.py
    Découpage de la méthode arrêt, ajout d'un booléen dans l'objet pour garantir une seule instance de PSR lancé

--- InterfaceEleve/ClassLancementPSR.py
+++ InterfaceEleve/ClassLancementPSR.py
@@ -20,6 +20,7 @@ class LancementPSR:
         """
         self.cheminEnregistrement = cheminEnregistrement
         self.nomFichier = nomFichier
+        self.estLancé = False
 
     @property
     def sortieFichier(self):
@@ -34,33 +35,50 @@ class LancementPSR:
             /gui 0 : cacher l'interface de PSR
             /output dossier de sortie des fichiers
         """
+        if (self.estLancé == False):
+             if self.nomFichier.length > 5:
+                 print("Le nom du fichier est trop long !")
 
-        if self.nomFichier.length > 5:
-            print("Le nom du fichier est trop long !")
+             #C:\Windows\System32\psr.exe
+             subprocess.Popen(["psr" , "/start" , "/arcxml", "1" , "/sc", "0", "/gui", "0" , "/output" , self.sortieFichier], shell=True)
 
-        #C:\Windows\System32\psr.exe
-        subprocess.Popen(["psr" , "/start" , "/arcxml", "1" , "/sc", "0", "/gui", "0" , "/output" , self.sortieFichier], shell=True)
+             self.estLancé == True
+        else:
+             print("Le logiciel PSR est déjà lancé !")
+                
+    def __dezipage(self):
+         #Attendre que le fichier se créer avant de le déziper
+         time.sleep(3)
+
+          #Dézipage
+         with zipfile.ZipFile(self.sortieFichier, 'r') as zip_ref:
+               zip_ref.extractall(r"{0}\unzip".format(self.cheminEnregistrement)) #CHOISIR UN NOM DE DOSSIER
+
+    def __supprimerFichierMHT(self):
+         """
+         Supprimer tous les fichier .mht du zip
+         """
+         #Supprimer .HTM
+         dossier = '{0}\\unzip'.format(self.cheminEnregistrement)
+         listeDocs = os.listdir(dossier)
+
+        #Parcours des fichiers pour supprimer tous les .MHT
+         for fichier in listeDocs:
+             if fichier.endswith(".mht"):
+                 os.remove(os.path.join(dossier, fichier))
+                 print(fichier," supprimé avec succès")
+
 
     def arret(self):
         """
         Arrête PSR, et prépare les fichiers produits pour la fusion avec BKL
         """
-        #C:\Windows\System32\psr.exe
-        subprocess.Popen(["psr" , "/stop"],  shell=True)
+        if (self.estLancé == True):
+             #C:\Windows\System32\psr.exe
+             subprocess.Popen(["psr" , "/stop"],  shell=True)
 
-        #Attendre que le fichier se créer avant de le déziper
-        time.sleep(3)
+             self.__dezipage()
 
-        #Dézipage
-        with zipfile.ZipFile(self.sortieFichier, 'r') as zip_ref:
-            zip_ref.extractall(r"{0}\unzip".format(self.cheminEnregistrement)) #CHOISIR UN NOM DE DOSSIER
-
-        #Supprimer .HTM
-        dossier = '{0}\\unzip'.format(self.cheminEnregistrement)
-        listeDocs = os.listdir(dossier)
-
-        #Parcours des fichiers pour supprimer tous les .MHT
-        for fichier in listeDocs:
-            if fichier.endswith(".mht"):
-                os.remove(os.path.join(dossier, fichier))
-                print(fichier," supprimé avec succès")
+             self.__supprimerFichierMHT()
+        else:
+             print("Le logiciel PSR n'est pas lancé !")
