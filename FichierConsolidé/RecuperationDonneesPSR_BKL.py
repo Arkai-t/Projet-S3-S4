ef894bfad59afa5596b7cc2d0027160a21442456
Malo LM <58141930+MaloLM@users.noreply.github.com>
2020-02-02 23:58:38 +0100

    initialisation du fichier

--- FichierConsolidé/RecuperationDonneesPSR_BKL.py
+++ FichierConsolidé/RecuperationDonneesPSR_BKL.py
@@ -1,1 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  2 18:26:49 2020
 
+@author: Malo
+"""
+
+import os
+from lxml import etree
+
+class RecuperationDonnees:
+    """
+    Classe qui permet de récupérer les données récoltées de PSR et BLK.
+    Elle récupére les données d'un fichier PSR 
+    lui même consolidé par les données essentielles du BLK (horodatage et contenu du texte)
+    """
+    
+    
+    
