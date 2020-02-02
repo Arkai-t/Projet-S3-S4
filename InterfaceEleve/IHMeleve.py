03bece3a1329adcdb0df33e86988df3a7f78d997
EstebanG64 <57949585+EstebanG64@users.noreply.github.com>
2020-02-02 19:21:32 +0100

    Add files via upload
    Ajout de commentaire et lecture de la charte juridique depuis un fichier

--- InterfaceEleve/IHMeleve.py
+++ InterfaceEleve/IHMeleve.py
@@ -4,76 +4,52 @@ import tkinter.scrolledtext as tkst
      
     
 
-fenetreCommencerExercice = Tk()
+fenetreCommencerExercice = Tk() #Création de la fenêtre pour commencer l'exercice
 fenetreCommencerExercice.geometry('200x50')
 fenetreCommencerExercice.title("Système d'analyse de traces - Commencer l'exercice")
 
 def fenetreEnSavoirPlus(self):
-    fenetreESP = Tk()
-    fenetreESP.attributes("-topmost",True)
+    """
+    Cette méthode permet d'afficher la fenêtre montrant la charte juridique du projet
+    """
+    monFichier = open("charteJuridique.txt","r") #Ouverture en mode lecture du fichier contenant la charte juridique
+    fenetreESP = Tk() #création de la fenêtre
+    fenetreESP.attributes("-topmost",True) #Permet de donner le focus à cette fenêtre
     fenetreESP.title("En savoir plus")
-    editArea = tkst.ScrolledText(master = fenetreESP, wrap = WORD)
+    editArea = tkst.ScrolledText(master = fenetreESP, wrap = WORD) #Création de la zone contenant le texte avec une scrollbar
     editArea.pack(padx=10,pady=10,fill=BOTH,expand=True)
-    editArea.insert(INSERT,
-                    """\
-POLITIQUE DE PROTECTION DES DONNÉES PERSONNELLES
-Dernière mise à jour : 03/12/2019
-                    
-    L’IUT de Bayonne et du Pays Basque est soucieux de la protection des données personnelles. Il met en œuvre une démarche d'amélioration continue de sa conformité au Règlement général de protection des données (RGPD), à la Directive ePrivacy, ainsi qu'à la loi n° 78-17 du 6 janvier 1978 dite Informatique et Libertés pour assurer le meilleur niveau de protection à vos données personnelles.\nPour toute information sur la protection des données personnelles, vous pouvez également consulter le site de la Commission Nationale de l'Informatique et des Libertés www.cnil.fr.\n\nQui est le responsable du traitement de mes données personnelles ?
-                    
-    Le responsable de traitement est la société qui définit pour quel usage et comment vos données personnelles sont utilisées.
-Les données personnelles collectées sur l’application d’analyse de traces sont traitées par :\nIUT de Bayonne et du Pays Basque
-Domiciliées: 2 Allée du Parc Montaury, 64600 Anglet
-Ci-après nommé : IUT
-
-
-Pourquoi l’IUT collecte mes données personnelles ?
-
-L’IUT utilise vos données personnelles principalement pour les finalités suivantes :
-    La pédagogie : Nous avons besoin des informations qui vous concerne pour améliorer l’évaluation vous concernant lors de votre parcours au sein de l’IUT informatique.
-    
-
-Quelles sont les données personnelles qui sont collectées me concernant ?
-
-Quelles données ?
-
-    Nous collectons et traitons notamment vos clics, sites consultés, application, mouvements de souris, textes saisis. Dans certains cas, nous pouvons collecter des données de .
-Toutes ces données sont collectées automatiquement du fait de vos actions sur l’application.
-
-Quand ?
-
-Nous collectons les informations que vous nous fournissez quand :
-    vous commencez un exercice, et après avoir recueilli votre consentement.
-    
-Quelles sont les communications que je suis susceptible de recevoir ?
-
-Les informations
-Statistiques : Suite à un exercice ou d’un contrôle, vous pourriez recevoir un email afin de vous annoncez les statistiques générales dudit exercice ou contrôle; ou bien lors d’un cours en amphithéâtre où le professeur vous fera le retour.
-                    """)
-    boutonFermer= Button(fenetreESP,text="Fermer", command=fenetreESP.destroy)
-    boutonFermer.pack(side=RIGHT)
+    editArea.insert(INSERT,monFichier.read())
+    boutonFermer= Button(fenetreESP,text="Fermer", command=fenetreESP.destroy) #Création du bouton permettant de fermer la fenêtre
+    boutonFermer.pack(side=RIGHT) #placement du bouton a droite de la fenêtre
     fenetreESP.pack()
+    monFichier.close() #Fermeture du fichier
 
-def command():
-    fenetreAutorisation = Tk()
+def affichageFenetreAutorisation():
+    """
+    Cette méthode permet d'afficher la fenêtre demandant l'autorisation de l'étudiant sur la collecte de ces traces.
+    Si l'étudiant accepte, cette méthode affichera la fenêtre permettant de mettre en pause la collecte et lancera les différents logiciels de capture de traces
+    Sinon cette méthode fermera l'application
+    """
+    fenetreCommencerExercice.destroy() #Ferme la fenêtre précédente
+    fenetreAutorisation = Tk() #Création de la fenêtre demandant l'autorisation
     fenetreAutorisation.title("New Window")
     label = Label(fenetreAutorisation, text="Acceptez-vous que vos données soient enregistrées dans un cadre pédagogique ?")
     label.pack()
     
-    labelEnSavoirPlus = Label(fenetreAutorisation, text="En savoir plus",fg="blue")
+    labelEnSavoirPlus = Label(fenetreAutorisation, text="En savoir plus",fg="blue") #Création du lien hypertexte qui ouvrira la fenêtre avec la charte juridique
     labelEnSavoirPlus.pack(side = LEFT)
     myfont = tkFont.Font(labelEnSavoirPlus, labelEnSavoirPlus.cget("font"))
     myfont.configure(underline = True)
     labelEnSavoirPlus.configure(font=myfont)
     labelEnSavoirPlus.bind("<Button-1>", fenetreEnSavoirPlus)
 
-    boutonAccepter = Button(fenetreAutorisation,text="J'accepte", command=fenetreAutorisation.destroy)
+    boutonAccepter = Button(fenetreAutorisation,text="J'accepte", command=fenetreAutorisation.destroy) #Création du bouton permettant à l'étudiant d'accepter la collecte des données
     boutonAccepter.pack(side=RIGHT)
-    boutonRefuser= Button(fenetreAutorisation,text="Je refuse", command=fenetreAutorisation.destroy)
+    boutonRefuser= Button(fenetreAutorisation,text="Je refuse", command=fenetreAutorisation.destroy) #Création du bouton permettant à l'étudiant de refuser la collecte des données
     boutonRefuser.pack(side=RIGHT)
 
     fenetreAutorisation.mainloop()
      
-newwindow = Button(fenetreCommencerExercice, text="Commencer exercice", command=command)
-newwindow.pack()
+boutonCommencerExercice = Button(fenetreCommencerExercice, text="Commencer exercice", command=affichageFenetreAutorisation)
+boutonCommencerExercice.pack()
 fenetreCommencerExercice.mainloop()
