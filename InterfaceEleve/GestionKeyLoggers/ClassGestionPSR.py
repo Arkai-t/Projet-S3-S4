7e679a558602169be18e956eba2898235a232790
Arkait53 <57949645+Arkait53@users.noreply.github.com>
2020-03-12 18:13:22 +0100

    Update ClassGestionPSR.py
    Délocalisation du traitement et du dézipage du fichier de PSR

--- InterfaceEleve/GestionKeyLoggers/ClassGestionPSR.py
+++ InterfaceEleve/GestionKeyLoggers/ClassGestionPSR.py
@@ -5,9 +5,6 @@ Created on Tue Jan 28 18:51:51 2020
 """
 
 import subprocess
-import zipfile
-import os
-from os import path
 
 #Location de PSR
 pathPSR = "C:/Windows/System32/psr.exe"
@@ -41,49 +38,8 @@ class GestionPSR:
 
         subprocess.Popen([pathPSR , "/start" , "/arcxml", "1" , "/sc", "0", "/gui", "0" , "/output" , self.sortieZip], shell=True)
 
-    def __deziper(self):
-         #Dézipage
-         with zipfile.ZipFile(self.sortieZip, 'r') as zip_ref:
-               zip_ref.extractall(r"{0}/{1}".format(self.cheminEnregistrement, self.nomFichier))
-
-         #Suppression du .zip
-         os.remove(self.sortieZip)
-
-    def __supprimer(self):
-         """
-         Supprimer tous les fichier .mht du dossier extrait du zip
-         """
-         listeDocs = os.listdir(self.sortieFichier)
-
-        #Parcours des fichiers pour supprimer tous les .MHT
-         for fichier in listeDocs:
-             if fichier.endswith(".mht"):
-                 os.remove(os.path.join(self.sortieFichier, fichier))
-
-    def __renommer(self):
-        """
-        Renomme l'unique fichier XML généré
-        """
-        listeDocs = os.listdir(self.sortieFichier)
-
-        for fichier in listeDocs:
-             if fichier.endswith(".xml"):
-                 os.rename((self.sortieFichier + fichier), (self.sortieFichier + self.nomFichier + ".xml"))
-             #Génére une erreur si abscence de break => essaye de renommer le fichier qui vient d'être renommé
-             break
-
     def arreter(self):
         """
         Arrête PSR, et prépare les fichiers produits pour la fusion avec BKL
         """
         subprocess.Popen([pathPSR , "/stop"],  shell=True)
-
-        #Attendre que PSR créé le zip
-        while path.isfile(self.sortieZip) != True:
-            pass
-
-        self.__deziper()
-
-        self.__supprimer()
-
-        self.__renommer()
