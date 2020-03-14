4ef2a32b2eee2e99180f412e6dd6a88e24da2de1
plafourca004 <57951515+plafourca004@users.noreply.github.com>
2020-03-14 21:50:33 +0100

    Modif de GestionBKL
    Fusion avec AjoutBKLaPSR
    Renommage de beaucoup de méthodes
    Renommage de la classe
    Des tests restent à faire

--- FichierConsolidé/TraitementBKL/TraitementBasicKeyLogger.py
+++ FichierConsolidé/TraitementBKL/TraitementBasicKeyLogger.py
@@ -3,6 +3,9 @@
 Created on Thu Jan 23 19:03:22 2020
 @author: Paul Lafourcade
 """
+
+
+
 from lxml import etree
 import csv #utilisation de la bibliothèque native csv
 
@@ -45,11 +48,12 @@ class Mot:
     def getHeureFin(self):
         return self.heureFin
         
-
     
 
+   #remarque: demande encore toute une série de test avant d'etre classé comme opérationnel
 
-class BKLtoTxt:
+
+class GestionBKL:
 
     def __init__(self,nomFichier):
         
@@ -63,7 +67,7 @@ class BKLtoTxt:
         self.listeMots = []
     
 
-    def recupererPhrases(self):
+    def __recupererPhrases(self):
         
         """
         On parcours le tableau  entièrement afin d'en récupérer les suites de lettres qui formeront des phrases
@@ -73,9 +77,9 @@ class BKLtoTxt:
             reader = csv.reader(tsvfile, delimiter='\t') #ouverture en tsv
         
             for row in reader:
-                self.majStatutTexte(row[0])
-                if self.isCurrentlyText() == True :
-                    if self.isFirst() == True :
+                self.__majStatutTexte(row[0])
+                if self.__isCurrentlyText() == True :
+                    if self.__isFirst() == True :
                         self.nouveauMot = Mot()
                         self.nouveauMot.setHeureDebut(row[6][13:])
                     self.nouveauMot.setHeureFin(row[6][13:])
@@ -84,10 +88,59 @@ class BKLtoTxt:
                     if self.nouveauMot is not None:
                         self.listeMots.append(self.nouveauMot)
                         self.nouveauMot = None
-                    
-        return self.listeMots
     
-    def majStatutTexte(self,lettre):
+    def __heureToNb(self,heure): #string sous forme de date en nb
+        (h, m, s) = heure.split(':')
+        return int(h) * 3600 + int(m) * 60 + int(s)    
+         
+
+       
+    def ajoutDuBKLaPSR(self,nomFichierPSR, nomNouveauFichierXML):
+        """
+            Seule fonction a lancer
+        """
+        
+        self.listeMots = self.__recupererPhrases()
+        
+        #initialisation du xml
+        tree = etree.parse(nomFichierPSR) 
+        root = tree.getroot()
+        etree.tostring(root)
+        
+        for texteAction in root.xpath('/Report/UserActionData/RecordSession/EachAction/Action/text()'):
+            if texteAction == "Saisie au clavier":
+                self.__traitementDeUnePhrase(root, texteAction)
+                        
+        #enregistrement de l'xml modifié
+        etree.ElementTree(root).write(nomNouveauFichierXML, pretty_print = True, xml_declaration=True, encoding="utf-8")
+    
+    
+    
+    def __traitementDeUnePhrase(self,root, texteAction):
+        parent = texteAction.getparent().getparent() #Balise EachAction de l'action récupérée
+
+        action = texteAction.getparent() #Balise action en elle meme
+        
+        #temp de debut de l'action et fin (debut de la suivante)
+        debutActionPSR = parent.get("Time")
+        parentSuivant = parent.getnext()
+        nextActionPSR = parentSuivant.get("Time")
+        
+        
+        for mot in self.listeMots:
+            if (self.__heureToNb(mot.getHeureDebut()) >= self.__heureToNb(debutActionPSR) and (self.__heureToNb(mot.getHeureFin()) <= self.__heureToNb(nextActionPSR))):
+                
+                #creation de l'element de texte
+                baliseTxt = etree.Element("texte")
+                baliseTxt.set("heureDebut", mot.getHeureDebut())
+                baliseTxt.set("heureFin", mot.getHeureFin())
+                baliseTxt.text = mot.getMot()
+
+                parent.insert(parent.index(action)+1, baliseTxt) #insertion après l'action
+    
+        
+    
+    def __majStatutTexte(self,lettre):
         '''
         permet de maj les booléens permettant de connaitre le statut actuel du texte
         '''
@@ -101,11 +154,9 @@ class BKLtoTxt:
             self.txtBool = False
             self.first = False
     
-    def isCurrentlyText(self):
+    def __isCurrentlyText(self):
         return self.txtBool
     
-    def isFirst(self):
+    def __isFirst(self):
         return self.first
-
-
 
