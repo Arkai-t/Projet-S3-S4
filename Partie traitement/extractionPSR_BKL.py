9886abe903863f0d7ec95a2c506ba996f55d4f0b
plafourca004 <57951515+plafourca004@users.noreply.github.com>
2020-03-29 17:42:12 +0200

    Ajout des actions de type enregistrement / compilation
    Refaire

--- Partie traitement/extractionPSR_BKL.py
+++ Partie traitement/extractionPSR_BKL.py
@@ -2,14 +2,31 @@
 """
 Created on Tue Mar 24 11:28:03 2020
 
-@author: paul6
+@author: Paul Lafourcade, Malo Le Mestre
 """
 from lxml import etree
 
-from classLogiciels import Logiciel
-from classLogiciels import Action
-from classLogiciels import Session
 
+from classLogiciels import *
+
+
+def isEnregistrer(texte):
+    if texte == "Enregistrer":
+        return True  
+    elif texte == "Save":
+        return True  
+    elif texte == "Enregistrer Sous":
+        return True
+    else:
+        return False
+
+def isCompiler(texte):
+    if texte == "Compiler":
+        return True  
+    elif texte == "Build":
+        return True
+    else:
+        return False
 
 
 sessionTP = Session()
@@ -25,7 +42,6 @@ attrSession = session[0].items() #get all recordsession attributes
 
 sessionTP.heureDebut = attrSession[1][1]
 sessionTP.heureFin = attrSession[2][1]
-sessionTP.actionCount = attrSession[3][1]
 
 listeEachActions = session[0].getchildren()
 
@@ -39,39 +55,119 @@ for action in listeEachActions:
     
     #remplir les actions
     
-    nouvelleAction = Action()
     
     balisesDeEachAction = action.getchildren()
     
-    for balise in balisesDeEachAction:
-        if balise.tag == "Action":
-            nouvelleAction.type = balise.text
-        if balise.tag == "Description":
-            nouvelleAction.description = balise.text
-        if balise.tag == "UIAStack":
-            #Traitement du UIAStack
-            levels = balise.getchildren()
-            for level in levels: #parcours de toutes les balises level   any('Name' in attr[0] for attr in levelAttributs
-                levelAttributs = level.items()
-                if nouvelleAction.localisationAction == "": #prendre le premier qui a une balise Name
-                    for attribut in levelAttributs:
-                        if attribut[0] == "Name":
-                            nouvelleAction.localisationAction = attribut[1]
-                if nouvelleAction.nomPage == "":
-                    for i, attribut in enumerate(levelAttributs):
-                        if attribut[0] == "LocalizedControlType" and attribut[1] == "fenêtre":
-                            nouvelleAction.nomPage = levelAttributs[i-1][1]
-        if balise.tag == "texte":
-            texteAttributs = balise.items()
-            nouvelleAction.addMot(balise.text, texteAttributs[0][1], texteAttributs[1][1])
+    isActionSpeciale = False
+    
+    
+    for baliseAction in balisesDeEachAction:
+        if baliseAction.tag == "Action":
+            typeAction = baliseAction.text.split(" ",1)[0]
+            if typeAction == "Clic": #Savoir si c'est clic
+                #Savoir de quel type d'action de clic est l'action
+                for UIAStack in balisesDeEachAction: 
+                    if UIAStack.tag == "UIAStack":
+                        for level in UIAStack.getchildren(): #Chaque balise level
+                            for attribut in level.items():  #chaque attributs de la balise level
+                                if attribut[0] == "Name":
+                                    if isEnregistrer(attribut[1]):
+                                        nouvelleAction = ActionEnregistrer()
+                                        print("L'action est un enregistrement")
+                                        isActionSpeciale = True
+                                        break
+                                    if isCompiler(attribut[1]):
+                                        nouvelleAction = ActionCompiler()
+                                        print("L'action est une compilation")
+                                        isActionSpeciale = True
+                                        break
+                            
+                        break
+                    
+                if isActionSpeciale == False:
+                    nouvelleAction = ActionClic()              
+                    print("L'action est un clic simple")
+                    
+                #Remplir
+                for balise in balisesDeEachAction:
+                    if balise.tag == "Action":
+                        nouvelleAction.setType(balise.text)
+                    if balise.tag == "Description":
+                        nouvelleAction.description = balise.text
+                    if balise.tag == "UIAStack":
+                        #Traitement du UIAStack
+                        levels = balise.getchildren()
+                        for level in levels: #parcours de toutes les balises level   any('Name' in attr[0] for attr in levelAttributs
+                            levelAttributs = level.items()
+                            if nouvelleAction.nomPage == "":
+                                for i, attribut in enumerate(levelAttributs):
+                                    if attribut[0] == "LocalizedControlType" and attribut[1] == "fenêtre":
+                                        nouvelleAction.nomPage = levelAttributs[i-1][1]
+                
+                nouvelleAction.heureDebut = heureAction
+                
+                nouveauLogiciel.listeActions.append(nouvelleAction)
+                nouveauLogiciel.setHeureDebut()
+                
+            #Si c'est un texte
+            if typeAction == "Saisie":
+                for baliseTexte in balisesDeEachAction:
+                    if baliseTexte.tag == "texte":
+                        #Création et remplissage de l'actionSaisie
+                        nouvelleAction = ActionSaisie()
+                        
+                        attributsTextes = baliseTexte.items()
+                        for balise in balisesDeEachAction:
+                            if balise.tag == "Action":
+                                nouvelleAction.setType(balise.text)
+                            if balise.tag == "Description":
+                                nouvelleAction.description = balise.text
+                                     
+                            if balise.tag == "UIAStack":
+                                #Traitement du UIAStack
+                                levels = balise.getchildren()
+                                for level in levels: #parcours de toutes les balises level   any('Name' in attr[0] for attr in levelAttributs
+                                    levelAttributs = level.items()
+                                    if nouvelleAction.nomPage == "":
+                                        for i, attribut in enumerate(levelAttributs):
+                                            if attribut[0] == "LocalizedControlType" and attribut[1] == "fenêtre":
+                                                nouvelleAction.nomPage = levelAttributs[i-1][1]
+                        
+                        nouvelleAction.heureDebut = attributsTextes[0][1]  
+                        nouvelleAction.phrase = baliseTexte.text
+                        
+                        nouveauLogiciel.listeActions.append(nouvelleAction)
+                        nouveauLogiciel.setHeureDebut()
+                        
+                        
+                        print("Action saisie cree")
+                        
+                        #Determiner si c'est un enregistrement en cherchant "CTRL-S" dans la chaine de caracteres
+                        if "Ctrl+S" in baliseTexte.text:
+                            
+                            nouvelleActionEnregistrer = ActionEnregistrer()
+                            nouvelleActionEnregistrer.setType("Enregistrement") #Sera set grace à l'héritage à Enregistrement quoi que ce soit
+                            nouvelleActionEnregistrer.setDescription("L'utilisateur a enregistré en appuyant sur Ctrl+S")
+                            nouvelleActionEnregistrer.heureDebut = attributsTextes[0][1]
+                            print("Action enregistrement saisie cree")
+                            for balise in balisesDeEachAction:   
+                                if balise.tag == "UIAStack":
+                                    #Traitement du UIAStack
+                                    levels = balise.getchildren()
+                                    for level in levels: #parcours de toutes les balises level   any('Name' in attr[0] for attr in levelAttributs
+                                        levelAttributs = level.items()
+                                        if nouvelleActionEnregistrer.nomPage == "":
+                                            for i, attribut in enumerate(levelAttributs):
+                                                if attribut[0] == "LocalizedControlType" and attribut[1] == "fenêtre":
+                                                    nouvelleActionEnregistrer.nomPage = levelAttributs[i-1][1]
+                        
+                            nouveauLogiciel.listeActions.append(nouvelleActionEnregistrer)
+                            nouveauLogiciel.setHeureDebut()
+                        
             
-        
-    nouvelleAction.heureDebut = heureAction
-    
-    
-    nouveauLogiciel.listeActions.append(nouvelleAction)
-    nouveauLogiciel.setHeureDebut()
-    
+            break
+
+
     #ajout (ou modif du logiciel)
     if (sessionTP.listeLogiciels):
         if(nouveauLogiciel.nom in sessionTP.getNoms()):
@@ -94,8 +190,8 @@ for logiciel in sessionTP.listeLogiciels:
     for action in logiciel.listeActions:
         print("- Nom de l'action: ", action.type, " (", action.heureDebut,")")
         print("   Description: ", action.description)
-        print("   Localisation: ", action.localisationAction)
         print("   Nom de la Page: ", action.nomPage)
-        print("   Mots: ")
-        for mot in action.mots:
-            print("     ", mot[0], " (", mot[1], " - ", mot[2], ")")
+        if isinstance(action ,ActionSaisie):
+            print("   Phrase: ", action.phrase)     
+        print("--")
+                
