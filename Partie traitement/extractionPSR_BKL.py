511c35f9f3781b3d20470dabee689c2ff4e3b0dd
Malo LM <58141930+MaloLM@users.noreply.github.com>
2020-03-30 12:31:50 +0200

    Optimisation du code
    usage de la fonction getSpecificChild()

--- Partie traitement/extractionPSR_BKL.py
+++ Partie traitement/extractionPSR_BKL.py
@@ -7,7 +7,7 @@ Created on Tue Mar 24 11:28:03 2020
 from lxml import etree
 
 
-from classLogiciel import *
+from ClassLogiciel import *
 
 
 def isEnregistrer(texte):
@@ -38,7 +38,7 @@ sessionTP = Session()
 
 #ouverture du xml
 
-tree = etree.parse("wow.xml")
+tree = etree.parse("testFichierFusionné.xml")
 
 #infos de session
 session = tree.xpath("/Report/UserActionData/RecordSession")
@@ -65,107 +65,100 @@ for action in listeEachActions:
     
     isActionSpeciale = False
     
-    
-    for baliseAction in balisesDeEachAction:
-        if baliseAction.tag == "Action":
-            typeAction = baliseAction.text.split(" ",1)[0]
-            if typeAction == "Clic": #Savoir si c'est clic
-                #Savoir de quel type d'action de clic est l'action
-                UIAStack = getSpecificChild(action, "UIAStack")
-                for level in UIAStack.getchildren(): #Chaque balise level
-                    for attribut in level.items():  #chaque attributs de la balise level
-                        if attribut[0] == "Name":
-                            if isEnregistrer(attribut[1]):
-                                nouvelleAction = ActionEnregistrer()
-                                isActionSpeciale = True
-                                break
-                            if isCompiler(attribut[1]):
-                                nouvelleAction = ActionCompiler()
-                                isActionSpeciale = True
-                                break
-                    
+    baliseAction = getSpecificChild(action,"Action")
+    typeAction = baliseAction.text.split(" ",1)[0]
+    if typeAction == "Clic": #Savoir si c'est clic
+        #Savoir de quel type d'action de clic est l'action
+        UIAStack = getSpecificChild(action, "UIAStack")
+        for level in UIAStack.getchildren(): #Chaque balise level
+            for attribut in level.items():  #chaque attributs de la balise level
+                if attribut[0] == "Name":
+                    if isEnregistrer(attribut[1]):
+                        nouvelleAction = ActionEnregistrer()
+                        isActionSpeciale = True
+                        break
+                    if isCompiler(attribut[1]):
+                        nouvelleAction = ActionCompiler()
+                        isActionSpeciale = True
+                        break
+            
+        if isActionSpeciale == False:
+            nouvelleAction = ActionClic()          
+            
+        #Remplir
+        balise = getSpecificChild(action,"Action")
+        nouvelleAction.setType(balise.text)
+        balise = getSpecificChild(action,"Description")        
+        nouvelleAction.description = balise.text
+        balise = getSpecificChild(action,"UIAStack")
+        #Traitement du UIAStack
+        levels = balise.getchildren()
+        for level in levels: #parcours de toutes les balises level   any('Name' in attr[0] for attr in levelAttributs
+            levelAttributs = level.items()
+            if nouvelleAction.nomPage == "":
+                for i, attribut in enumerate(levelAttributs):
+                    if attribut[0] == "LocalizedControlType" and attribut[1] == "fenêtre":
+                        nouvelleAction.nomPage = levelAttributs[i-1][1]
+        
+        nouvelleAction.heureDebut = heureAction
+        nouvelleAction.setNom()
+        nouveauLogiciel.listeActions.append(nouvelleAction)
+        nouveauLogiciel.setHeureDebut()
+        
+    #Si c'est un texte
+    if typeAction == "Saisie":
+        for baliseTexte in balisesDeEachAction:
+            if baliseTexte.tag == "texte":
+                #Création et remplissage de l'actionSaisie
+                nouvelleAction = ActionSaisie()
+                attributsTextes = baliseTexte.items()
+                balise = getSpecificChild(action,"Action")
+                nouvelleAction.setType(balise.text)
+                balise = getSpecificChild(action,"Description")        
+                nouvelleAction.description = balise.text
+                balise = getSpecificChild(action,"UIAStack")
+                #Traitement du UIAStack
+                levels = balise.getchildren()
+                for level in levels: #parcours de toutes les balises level   any('Name' in attr[0] for attr in levelAttributs
+                    levelAttributs = level.items()
+                    if nouvelleAction.nomPage == "":
+                        for i, attribut in enumerate(levelAttributs):
+                            if attribut[0] == "LocalizedControlType" and attribut[1] == "fenêtre":
+                                nouvelleAction.nomPage = levelAttributs[i-1][1]
                 
-                    
-                if isActionSpeciale == False:
-                    nouvelleAction = ActionClic()          
-                    
-                #Remplir
-                for balise in balisesDeEachAction:
-                    if balise.tag == "Action":
-                        nouvelleAction.setType(balise.text)
-                    if balise.tag == "Description":
-                        nouvelleAction.description = balise.text
-                    if balise.tag == "UIAStack":
-                        #Traitement du UIAStack
-                        levels = balise.getchildren()
-                        for level in levels: #parcours de toutes les balises level   any('Name' in attr[0] for attr in levelAttributs
-                            levelAttributs = level.items()
-                            if nouvelleAction.nomPage == "":
-                                for i, attribut in enumerate(levelAttributs):
-                                    if attribut[0] == "LocalizedControlType" and attribut[1] == "fenêtre":
-                                        nouvelleAction.nomPage = levelAttributs[i-1][1]
-                
-                nouvelleAction.heureDebut = heureAction
-                
+                nouvelleAction.heureDebut = attributsTextes[0][1]  
+                nouvelleAction.phrase = baliseTexte.text
+                nouvelleAction.setNom()
                 nouveauLogiciel.listeActions.append(nouvelleAction)
                 nouveauLogiciel.setHeureDebut()
                 
-            #Si c'est un texte
-            if typeAction == "Saisie":
-                for baliseTexte in balisesDeEachAction:
-                    if baliseTexte.tag == "texte":
-                        #Création et remplissage de l'actionSaisie
-                        nouvelleAction = ActionSaisie()
-                        
-                        attributsTextes = baliseTexte.items()
-                        for balise in balisesDeEachAction:
-                            if balise.tag == "Action":
-                                nouvelleAction.setType(balise.text)
-                            if balise.tag == "Description":
-                                nouvelleAction.description = balise.text
-                                     
-                            if balise.tag == "UIAStack":
-                                #Traitement du UIAStack
-                                levels = balise.getchildren()
-                                for level in levels: #parcours de toutes les balises level   any('Name' in attr[0] for attr in levelAttributs
-                                    levelAttributs = level.items()
-                                    if nouvelleAction.nomPage == "":
-                                        for i, attribut in enumerate(levelAttributs):
-                                            if attribut[0] == "LocalizedControlType" and attribut[1] == "fenêtre":
-                                                nouvelleAction.nomPage = levelAttributs[i-1][1]
-                        
-                        nouvelleAction.heureDebut = attributsTextes[0][1]  
-                        nouvelleAction.phrase = baliseTexte.text
-                        
-                        nouveauLogiciel.listeActions.append(nouvelleAction)
-                        nouveauLogiciel.setHeureDebut()
-                        
-                        
-                        print("Action saisie cree")
-                        
-                        #Determiner si c'est un enregistrement en cherchant "CTRL-S" dans la chaine de caracteres
-                        if "Ctrl+S" in baliseTexte.text:
-                            
-                            nouvelleActionEnregistrer = ActionEnregistrer()
-                            nouvelleActionEnregistrer.setType("Enregistrement") #Sera set grace à l'héritage à Enregistrement quoi que ce soit
-                            nouvelleActionEnregistrer.setDescription("L'utilisateur a enregistré en appuyant sur Ctrl+S")
-                            nouvelleActionEnregistrer.heureDebut = attributsTextes[0][1]
-                            for balise in balisesDeEachAction:   
-                                if balise.tag == "UIAStack":
-                                    #Traitement du UIAStack
-                                    levels = balise.getchildren()
-                                    for level in levels: #parcours de toutes les balises level   any('Name' in attr[0] for attr in levelAttributs
-                                        levelAttributs = level.items()
-                                        if nouvelleActionEnregistrer.nomPage == "":
-                                            for i, attribut in enumerate(levelAttributs):
-                                                if attribut[0] == "LocalizedControlType" and attribut[1] == "fenêtre":
-                                                    nouvelleActionEnregistrer.nomPage = levelAttributs[i-1][1]
-                        
-                            nouveauLogiciel.listeActions.append(nouvelleActionEnregistrer)
-                            nouveauLogiciel.setHeureDebut()
-                        
-            
-            break
+                
+                print("Action saisie cree")
+                
+                #Determiner si c'est un enregistrement en cherchant "CTRL-S" dans la chaine de caracteres
+                if "Ctrl+S" in baliseTexte.text:
+                    
+                    nouvelleActionEnregistrer = ActionEnregistrer()
+                    nouvelleActionEnregistrer.setType("Enregistrement") #Sera set grace à l'héritage à Enregistrement quoi que ce soit
+                    nouvelleActionEnregistrer.setDescription("L'utilisateur a enregistré en appuyant sur Ctrl+S")
+                    nouvelleActionEnregistrer.heureDebut = attributsTextes[0][1]
+                    balise = getSpecificChild(action,"UIAStack")
+
+                    #Traitement du UIAStack
+                    levels = balise.getchildren()
+                    for level in levels: #parcours de toutes les balises level   any('Name' in attr[0] for attr in levelAttributs
+                        levelAttributs = level.items()
+                        if nouvelleActionEnregistrer.nomPage == "":
+                            for i, attribut in enumerate(levelAttributs):
+                                if attribut[0] == "LocalizedControlType" and attribut[1] == "fenêtre":
+                                    nouvelleActionEnregistrer.nomPage = levelAttributs[i-1][1]
+                                            
+                    nouvelleActionEnregistrer.setNom()                    
+                    nouveauLogiciel.listeActions.append(nouvelleActionEnregistrer)
+                    nouveauLogiciel.setHeureDebut()
+                
+    
+ 
 
 
     #ajout (ou modif du logiciel)
@@ -180,6 +173,7 @@ for action in listeEachActions:
         else:
             sessionTP.listeLogiciels.append(nouveauLogiciel)        
     else:
+        nouvelleAction.setNom()
         sessionTP.listeLogiciels.append(nouveauLogiciel)
         
         
@@ -188,7 +182,8 @@ for logiciel in sessionTP.listeLogiciels:
     print("------------------------------------")
     print("Logiciel : ", logiciel.nom, " (", logiciel.heureDebut,")")
     for action in logiciel.listeActions:
-        print("- Nom de l'action: ", action.type, " (", action.heureDebut,")")
+        print("-  Nom de l'action: ", action.nom, " ")
+        print("   Type de l'action: ", action.type, " (", action.heureDebut,")")
         print("   Description: ", action.description)
         print("   Nom de la Page: ", action.nomPage)
         if isinstance(action ,ActionSaisie):
