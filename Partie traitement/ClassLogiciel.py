e74c1b97f8094359837ea1f362f977fc165b82f8
Malo LM <58141930+MaloLM@users.noreply.github.com>
2020-03-30 14:26:00 +0200

    enrichissement des classes
    ajout de méthodes

--- Partie traitement/ClassLogiciel.py
+++ Partie traitement/ClassLogiciel.py
@@ -1,19 +1,33 @@
-# -*- coding: utf-8 -*-
-"""
-@author: paul6,Malo LM, Louison V
-"""
-# -*- coding: utf-8 -*-
+
+
+
 class Action:
+    
     def __init__(self):
         self.nom = ""
         self.type = ""
         self.description = ""
         self.heureDebut = ""
         self.nomPage = ""
-
+      
+   
     def setDescription(self, desc):
         self.description = desc
+        
+    def getType(self):
+         return self.type
 
+    def getHeureDebut(self):
+         return self.heureDebut   
+     
+    def setNomPageFromUIAStack(self, balise):
+        levels = balise.getchildren()
+        for level in levels: #parcours de toutes les balises level   any('Name' in attr[0] for attr in levelAttributs
+            levelAttributs = level.items()
+            if self.nomPage == "":
+                for i, attribut in enumerate(levelAttributs):
+                    if attribut[0] == "LocalizedControlType" and attribut[1] == "fenêtre":
+                        self.nomPage = levelAttributs[i-1][1] 
     def getNom(self):
          return self.nom
 
@@ -27,34 +41,36 @@ class Action:
          return self.heureDebut
 
     def getNomPage(self):
-         return self.nomPage
-
-
+         return self.nomPage                    
+        
+     
 
 class ActionClic(Action):
     def __init__(self):
         Action.__init__(self)
-
+        
     def setType(self, typeAction):
         self.type = typeAction
-
     def setNom(self):
         self.nom = "Clic"
+        
+
 
 
 class ActionSaisie(Action):
     def __init__(self):
         Action.__init__(self)
         self.phrase = ""
-
+        
     def setType(self, typeAction):
         self.type = typeAction
-
+        
     def setNom(self):
-        self.nom = "Saisie"
-
+        self.nom = "Saisie"  
+        
     def getPhrase(self):
-         return self.phrase
+         return self.phrase    
+        
 
 
 class ActionEnregistrer(Action):
@@ -63,43 +79,49 @@ class ActionEnregistrer(Action):
 
     def setType(self, typeAction):
         self.type = "Enregistrement"
-
     def setNom(self):
-        self.nom = "Enregistrement"
-
-
+        self.nom = "Enregistrement"    
+    
 class ActionCompiler(Action):
     def __init__(self):
         Action.__init__(self)
-
+        
     def setType(self, typeAction):
         self.type = "Compilation"
-
     def setNom(self):
-        self.nom = "Compilation"
+        self.nom = "Compilation"  
+        
 
 #-------------------------------------------------------------------------
 
 class Logiciel:
-
+    
     def __init__(self,nom):
         self.nom = nom
         self.heureDebut = "24:00:00"
         self.listeActions = []
-        self.tempsPasse = ""
-
+        self.tempsPasse = 0
+    
     def setHeureDebut(self):
         heurePlusPetite = "24:00:00"
         for action in self.listeActions:
             if self._heureToNb(action.heureDebut) < self._heureToNb(heurePlusPetite):
                 heurePlusPetite = action.heureDebut
         self.heureDebut = heurePlusPetite
-
-
+        
+    def addAction(self, action, temps):
+        self.listeActions.append(action)
+        self.tempsPasse = self.tempsPasse + temps
+        print ("temps passé depuis addaction : ",self.tempsPasse, "pour l'action : ", action.type)
+        
+    
     def _heureToNb(self,heure):
         (h, m, s) = heure.split(':')
-        return int(h) * 3600 + int(m) * 60 + int(s)
-
+        return int(h) * 3600 + int(m) * 60 + int(s)  
+    
+    #def _secToHeure(self,secondes):
+        
+    
     def getNom(self):
          return self.nom
 
@@ -111,27 +133,28 @@ class Logiciel:
 
     def getActions(self):
          return self.listeActions
-
+     
     def getTempsPasse(self):
          return self.tempsPasse
-
+    
+        
 #--------------------------------------------------------------------------
-
+    
 class Session:
     def __init__(self):
         self.listeLogiciels = []
         self.heureDebut = ""
         self.heureFin = ""
-
+        
     def addLogiciel(self,logiciel):
         self.listeLogiciels.append(logiciel)
-
+        
     def getNoms(self):
         tabNoms = []
         for logiciel in self.listeLogiciels:
             tabNoms.append(logiciel.nom)
         return tabNoms
-
+            
     def getHeureDebut(self):
          return self.heureDebut
 
@@ -140,3 +163,5 @@ class Session:
 
     def getLogiciels(self):
          return self.listeLogiciels
+        
+        
