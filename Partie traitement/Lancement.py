be7f8a193985e873df6f53fa19eb7f7a2b9a5bfd
Arkait53 <57949645+Arkait53@users.noreply.github.com>
2020-03-31 17:32:43 +0200

    Suppression de print

--- Partie traitement/Lancement.py
+++ Partie traitement/Lancement.py
@@ -9,21 +9,13 @@ from ClassPreparationFichiers import PreparationFichiers
 from ClassAjoutBKLaPSR import FichierFusionne
 from ClassCreationFichierConsolide import FichierConsolide
 
-print('Preparation des fichiers en cours')
 gest = PreparationFichiers()
 gest.preparerFichiers()
-print('Preparation des fichiers effectuée')
 
-
-print('Creation du fichier fusionne en cours')
 bkl = FichierFusionne()
 bkl.ajoutDuBKLaPSR()
-print('Creation du fichier fusionne terminée')
 
-
-print('Creation du fichier consolidé en cours')
 monFicConsolide = FichierConsolide()
 monFicConsolide.recupererInformations()
 monFicConsolide.creerArborescence()
 monFicConsolide.sauvegardeFichier()
-print('Creation du fichier consolidé terminée')
