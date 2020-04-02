bd09399b4d2751f0feec05d907206799d4a09b08
plafourca004 <57951515+plafourca004@users.noreply.github.com>
2020-04-02 22:21:04 +0200

    Update Lancement.py

--- Partie traitement/Lancement.py
+++ Partie traitement/Lancement.py
@@ -9,12 +9,16 @@ from ClassPreparationFichiers import PreparationFichiers
 from ClassAjoutBKLaPSR import FichierFusionne
 from ClassCreationFichierConsolide import FichierConsolide
 
+
+#1
 gest = PreparationFichiers()
 gest.preparerFichiers()
 
+#2
 bkl = FichierFusionne()
 bkl.ajoutDuBKLaPSR()
 
+#3
 monFicConsolide = FichierConsolide()
 monFicConsolide.recupererInformations()
 monFicConsolide.creerArborescence()
